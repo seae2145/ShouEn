43767772f2f1d3668cd302aa8d0e5b945be666f5
scsa3 <scsa32@gmail.com>
2016-08-11 12:08:42 +0800

    Create ShouEn Folder

--- ShouEn/mssql_upload.py
+++ ShouEn/mssql_upload.py
@@ -1,9 +1,9 @@
 import pymssql
 
-conn = pymssql.connect(server='192.168.0.20', user='sa', password='csii1qaz@WSX', database='stipendiary')
-
 
 def upload(area, objectID, objectName, objectValue):
+    conn = pymssql.connect(server='192.168.0.20', user='sa', password='csii1qaz@WSX', database='stipendiary')
+
     cursor = conn.cursor()
     cursor.execute("INSERT INTO [dbo].[301] (area, objectID, objectName, objectValue) VALUES (%s, %s, %s, %s)",
                    (area, objectID, objectName, objectValue))
